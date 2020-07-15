f8403c83cf50841c9284e2b2c6aa9325eb21c72f
sangya2001 <sangyasherpa2058@gmail.com>
2020-07-15 21:53:57 +0545

    Still Working on Events

--- src/Dashboard.java
+++ src/Dashboard.java
@@ -96,31 +96,126 @@ public class Dashboard extends LoginRegistration {
     public void EventsFrame(ArrayList<Events> events){
         JFrame eventsFrame = new JFrame("Events");
 
+        // go back to dashboard
+        JButton goBack = new JButton("Go back");
+        goBack.setFocusPainted(false);
+        goBack.setOpaque(false);
+        goBack.setForeground(Color.BLUE);
+        goBack.setBounds(3, 15, 80, 30);
+        goBack.setContentAreaFilled(false);
+        goBack.setBorderPainted(false);
+
         int count = 0;
         while(events.size() > count){
             // event title
-            JLabel eventTitleLabel = new JLabel(events.get(count).eventTitle);
-
+            JLabel eventTitleLabel = new JLabel((count+1) + ". " + events.get(count).eventTitle);
             int yBoundTitle;
-
-            if(count == 0){
-                yBoundTitle = count * 50 + 40;
+            switch (count){
+                case 0: yBoundTitle = 40; break;
+                case 1: yBoundTitle = 200;break;
+                case 2: yBoundTitle = 340;break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + count);
             }
-//            else if(events.size()-1 == count){
-//                yBoundTitle = (events.size() - 1) * 50 + 80;
-//            }
-            else{
-                yBoundTitle = count * 50 + 80;
+            eventTitleLabel.setBounds(20, yBoundTitle, 150, 50);
+
+            // event Description
+            JLabel eventDescription = new JLabel(events.get(count).eventDescription);
+            int yBoundDescription;
+            switch (count){
+                case 0: yBoundDescription = 60; break;
+                case 1: yBoundDescription = 220;break;
+                case 2: yBoundDescription = 360;break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + count);
+            }
+            eventDescription.setBounds(20, yBoundDescription, 300, 50);
+            eventDescription.setForeground(Color.gray);
+
+            // event time
+            JLabel eventTime = new JLabel(events.get(count).eventTime);
+            int yBoundEventTime;
+            switch(count){
+                case 0: yBoundEventTime = 140; break;
+                case 1: yBoundEventTime = 300;break;
+                case 2: yBoundEventTime = 450;break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + count);
+            }
+            eventTime.setBounds(20, yBoundEventTime, 100, 50);
+            eventTime.setForeground(Color.gray);
+
+            // event date
+            JLabel eventDate = new JLabel(", " + events.get(count).eventDate);
+            int yBoundEventDate;
+            switch (count){
+                case 0: yBoundEventDate = 140; break;
+                case 1: yBoundEventDate = 300;break;
+                case 2: yBoundEventDate = 450;break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + count);
+            }
+            eventDate.setBounds(82, yBoundEventDate, 100, 50);
+            eventDate.setForeground(Color.gray);
+
+            // event Type
+            // if it is online event
+            if(events.get(count).isOnlineEvent == true){
+                JLabel eventType = new JLabel("Online Event");
+
+                eventType.setBounds(20, 100, 80, 50);
+                eventType.setForeground(Color.gray);
+
+                JLabel eventURL = new JLabel(events.get(count).eventURL);
+                eventURL.setBounds(20, 120, 250, 50);
+                eventURL.setForeground(Color.gray);
+
+                // adding components to frame
+                eventsFrame.add(eventType);
+                eventsFrame.add(eventURL);
+            }
+            else if(events.get(count).isExcursionEvent == true){// if external event
+                JLabel eventType = new JLabel("External Event");
+                eventType.setBounds(20, 260, 100, 50);
+                eventType.setForeground(Color.gray);
+
+                eventsFrame.add(eventType);
+            }
+            else if(events.get(count).isInternalEvent == true){// if internal event
+                JLabel eventType = new JLabel("Internal Event");
+
+                eventType.setBounds(20, 400, 80, 50);
+                eventType.setForeground(Color.gray);
+
+                eventsFrame.add(eventType);
             }
 
-            eventTitleLabel.setBounds(20, yBoundTitle, 105, 50);
+            // check if limited seat is available
+            if(events.get(count).isSeatLimited){
+                JLabel limitedSeat = new JLabel("Limited Seats: " + events.get(count).limitedSeats);
+                int yBoundLimitedSeats;
+                switch(count){
+                    case 0: yBoundLimitedSeats = 160;break;
+                    case 1: yBoundLimitedSeats = 320;break;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + count);
+                }
+                limitedSeat.setBounds(20, yBoundLimitedSeats, 200, 50);
+                limitedSeat.setForeground(Color.gray);
+                eventsFrame.add(limitedSeat);
+            }
 
             // adding components to the frame
             eventsFrame.add(eventTitleLabel);
+            eventsFrame.add(eventDescription);
+            eventsFrame.add(eventTime);
+            eventsFrame.add(eventDate);
             ++count;
         }
+
+        eventsFrame.add(goBack);
         // frame components
-        eventsFrame.setSize(350,380);//width and height
+        eventsFrame.setSize(350,580);//width and height
         eventsFrame.setLayout(null);//using no layout managers
         eventsFrame.setVisible(true);//making the frame visible
         eventsFrame.setLocationRelativeTo(null);//make the JFrame appear to center of screen
